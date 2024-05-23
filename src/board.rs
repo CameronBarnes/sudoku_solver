01d868ec64613133883b12f7c4812046c9857c7d
Cameron Barnes <cameron_barnes@outlook.com>
2024-05-22 17:28:35 -0700

    Implemented basic deductive solver

---
+++ src/board.rs
@@ -0,0 +1,169 @@
+use std::collections::HashSet;
+
+#[derive(Debug, Clone)]
+pub enum Cell {
+    Known(u8),
+    Possible(Vec<u8>),
+}
+
+impl Default for Cell {
+    fn default() -> Self {
+        Self::Possible((1..=9).collect())
+    }
+}
+
+impl Cell {
+    pub fn check(&mut self) {
+        match self {
+            Cell::Known(_) => {}
+            Cell::Possible(values) => {
+                if values.len() == 1 {
+                    *self = Self::Known(values[0]);
+                }
+            }
+        }
+    }
+
+    pub fn is_known(&self) -> bool {
+        match self {
+            Cell::Known(_) => true,
+            Cell::Possible(_) => false,
+        }
+    }
+
+    pub fn value(&self) -> Option<u8> {
+        match self {
+            Cell::Known(value) => Some(*value),
+            Cell::Possible(_) => None,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Board {
+    board: Vec<Vec<Cell>>,
+}
+
+impl Default for Board {
+    fn default() -> Self {
+        Self {
+            board: vec![vec![Cell::default(); 9]; 9],
+        }
+    }
+}
+
+impl Board {
+    fn rows(&self) -> Vec<Vec<&Cell>> {
+        (0..9).map(|row| self.row(row)).collect()
+    }
+
+    fn cols(&self) -> Vec<Vec<&Cell>> {
+        (0..9).map(|col| self.col(col)).collect()
+    }
+
+    fn groups(&self) -> Vec<Vec<&Cell>> {
+        (0..3)
+            .flat_map(move |row| (0..3).map(move |col| self.group(row, col)))
+            .collect()
+    }
+}
+
+impl Board {
+    pub fn col(&self, index: usize) -> Vec<&Cell> {
+        self.board
+            .iter()
+            .map(|row| row.get(index).unwrap())
+            .collect()
+    }
+
+    pub fn col_mut(&mut self, index: usize) -> Vec<&mut Cell> {
+        self.board
+            .iter_mut()
+            .map(|row| row.get_mut(index).unwrap())
+            .collect()
+    }
+
+    pub fn row(&self, index: usize) -> Vec<&Cell> {
+        self.board.get(index).unwrap().iter().collect()
+    }
+
+    pub fn row_mut(&mut self, index: usize) -> Vec<&mut Cell> {
+        self.board.get_mut(index).unwrap().iter_mut().collect()
+    }
+
+    pub fn get(&self, row: usize, col: usize) -> &Cell {
+        &self.board[row][col]
+    }
+
+    pub fn get_mut(&mut self, row: usize, col: usize) -> &mut Cell {
+        &mut self.board[row][col]
+    }
+
+    pub fn group(&self, row: usize, col: usize) -> Vec<&Cell> {
+        self.board
+            .iter()
+            .enumerate()
+            .filter_map(|(row_i, vals)| {
+                if row_i >= row * 3 && row_i < row * 3 + 3 {
+                    Some(vals)
+                } else {
+                    None
+                }
+            })
+            .flat_map(|row| row.iter().enumerate())
+            .filter_map(|(col_i, vals)| {
+                if col_i >= col * 3 && col_i < col * 3 + 3 {
+                    Some(vals)
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+
+    pub fn group_mut(&mut self, row: usize, col: usize) -> Vec<&mut Cell> {
+        self.board
+            .iter_mut()
+            .enumerate()
+            .filter_map(|(row_i, vals)| {
+                if row_i >= row * 3 && row_i < row * 3 + 3 {
+                    Some(vals)
+                } else {
+                    None
+                }
+            })
+            .flat_map(|row| row.iter_mut().enumerate())
+            .filter_map(|(col_i, vals)| {
+                if col_i >= col * 3 && col_i < col * 3 + 3 {
+                    Some(vals)
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+
+    pub fn is_solved(&self) -> bool {
+        self.board.iter().flatten().all(Cell::is_known)
+    }
+
+    pub fn is_correct(&self) -> bool {
+        self.rows().iter().all(|row| {
+            let mut check = HashSet::new();
+            row.iter()
+                .filter_map(|cell| cell.value())
+                .all(|val| val <= 9 && check.insert(val))
+        }) && self.cols().iter().all(|col| {
+            let mut check = HashSet::new();
+            col.iter()
+                .filter_map(|cell| cell.value())
+                .all(|val| val <= 9 && check.insert(val))
+        }) && self.groups().iter().all(|group| {
+            let mut check = HashSet::new();
+            group
+                .iter()
+                .filter_map(|cell| cell.value())
+                .all(|val| val <= 9 && check.insert(val))
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,162 @@
+mod board;
+
+use board::{Board, Cell};
+
+fn fixed(board: &mut Board) {
+    *board.get_mut(0, 2) = Cell::Known(4);
+    *board.get_mut(0, 3) = Cell::Known(8);
+    *board.get_mut(0, 5) = Cell::Known(5);
+    *board.get_mut(0, 6) = Cell::Known(7);
+    *board.get_mut(0, 8) = Cell::Known(6);
+
+    *board.get_mut(1, 3) = Cell::Known(9);
+    *board.get_mut(1, 7) = Cell::Known(4);
+
+    *board.get_mut(2, 1) = Cell::Known(2);
+    *board.get_mut(2, 3) = Cell::Known(4);
+    *board.get_mut(2, 8) = Cell::Known(1);
+
+    *board.get_mut(3, 0) = Cell::Known(8);
+    *board.get_mut(3, 3) = Cell::Known(7);
+    *board.get_mut(3, 5) = Cell::Known(3);
+    *board.get_mut(3, 7) = Cell::Known(6);
+
+    *board.get_mut(4, 2) = Cell::Known(6);
+    *board.get_mut(4, 5) = Cell::Known(1);
+    *board.get_mut(4, 6) = Cell::Known(5);
+
+    *board.get_mut(5, 0) = Cell::Known(2);
+    *board.get_mut(5, 5) = Cell::Known(8);
+    *board.get_mut(5, 6) = Cell::Known(4);
+    *board.get_mut(5, 7) = Cell::Known(7);
+
+    *board.get_mut(6, 1) = Cell::Known(4);
+    *board.get_mut(6, 3) = Cell::Known(5);
+    *board.get_mut(6, 5) = Cell::Known(6);
+
+    *board.get_mut(7, 0) = Cell::Known(5);
+    *board.get_mut(7, 6) = Cell::Known(6);
+    *board.get_mut(7, 7) = Cell::Known(8);
+
+    *board.get_mut(8, 3) = Cell::Known(1);
+    *board.get_mut(8, 7) = Cell::Known(5);
+    *board.get_mut(8, 8) = Cell::Known(4);
+}
+
+fn player_entered(board: &mut Board) {
+    for row in 0..9 {
+        for col in 0..9 {
+            let mut str = String::new();
+            std::io::stdin().read_line(&mut str).unwrap();
+            if str.trim().eq("0") {
+                continue;
+            } else {
+                *board.get_mut(row, col) = Cell::Known(str.trim().parse().unwrap());
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut board = Board::default();
+
+    //fixed(&mut board);
+    player_entered(&mut board);
+
+    while !board.is_solved() {
+        let mut updated = false;
+        for row in 0..9 {
+            if handle_collection(board.row_mut(row)) {
+                updated = true;
+            }
+        }
+
+        for col in 0..9 {
+            if handle_collection(board.col_mut(col)) {
+                updated = true;
+            }
+        }
+
+        for group_y in 0..3 {
+            for group_x in 0..3 {
+                if handle_collection(board.group_mut(group_y, group_x)) {
+                    updated = true;
+                }
+            }
+        }
+
+        if updated {
+            println!("updated");
+        } else {
+            dbg!(&board);
+            return;
+        }
+    }
+
+    println!("Done!");
+    if board.is_correct() {
+        println!("Solution is correct!");
+    } else {
+        println!("Solution is invalid!");
+    }
+}
+
+fn handle_collection(mut cells: Vec<&mut Cell>) -> bool {
+    let mut updated = false;
+    let mut present: Vec<u8> = cells
+        .iter()
+        .filter_map(|cell| match **cell {
+            Cell::Known(val) => Some(val),
+            Cell::Possible(_) => None,
+        })
+        .collect();
+    for cell in &mut cells {
+        let known = cell.is_known();
+        match cell {
+            Cell::Known(_) => {}
+            Cell::Possible(possible) => {
+                let len = possible.len();
+                possible.retain(|num| !present.contains(num));
+                if possible.len() != len {
+                    updated = true;
+                }
+            }
+        }
+        cell.check();
+        if known != cell.is_known() {
+            present.push(cell.value().unwrap());
+        }
+    }
+
+    for missing in 1..=9 {
+        if present.contains(&missing) {
+            continue;
+        }
+        let mut possible_match = 0;
+        for cell in &cells {
+            match cell {
+                Cell::Known(_) => {}
+                Cell::Possible(possible) => {
+                    if possible.contains(&missing) {
+                        possible_match += 1;
+                    }
+                }
+            }
+        }
+        if possible_match == 1 {
+            for cell in &mut cells {
+                match cell {
+                    Cell::Known(_) => {}
+                    Cell::Possible(possible) => {
+                        if possible.contains(&missing) {
+                            **cell = Cell::Known(missing);
+                        }
+                    }
+                }
+            }
+            present.push(missing);
+        }
+    }
+
+    updated
 }
